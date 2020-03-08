f3b620b941b104c3b751e6a3523f6aac33b7cee4
amgchv <amgchv@gmail.com>
2020-03-08 13:11:39 +0400

    added car healthchek scheduled job

--- car-service/src/main/java/com/sstu/carservice/Application.java
+++ car-service/src/main/java/com/sstu/carservice/Application.java
@@ -1,6 +1,7 @@
 package com.sstu.carservice;
 
 import com.sstu.carservice.appconfig.ApplicationConfig;
+import com.sstu.carservice.car.management.CarScheduler;
 import com.sstu.carservice.handler.ServerHandler;
 import com.sstu.carservice.model.ConfigModel;
 import io.netty.bootstrap.ServerBootstrap;
@@ -10,6 +11,10 @@ import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.quartz.SchedulerFactory;
+import org.quartz.impl.StdSchedulerFactory;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
@@ -17,8 +22,14 @@ import java.net.InetSocketAddress;
 public class Application {
 
     private static ConfigModel configModel = ApplicationConfig.getConfig();
+    private static CarScheduler carScheduler = new CarScheduler();
 
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws IOException, InterruptedException, SchedulerException {
+
+        SchedulerFactory schedulerFactory = new StdSchedulerFactory();
+        Scheduler scheduler = schedulerFactory.getScheduler();
+        scheduler.scheduleJob(carScheduler.getJob(), carScheduler.getTrigger());
+        scheduler.start();
 
         EventLoopGroup group = new NioEventLoopGroup();
         try {
